47698ef0aeaa316403c3e5f74bac071a5e33388b
gerom <gerome.bernabe@ucalgary.ca>
2022-11-15 23:03:57 -0700

    Worked on GUI, created a controller for the GUI

--- src/application/Main.java
+++ src/application/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
 			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("Country Guesser");
+			primaryStage.setTitle("Currency Converter");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
