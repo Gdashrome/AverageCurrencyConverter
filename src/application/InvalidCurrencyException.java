501bc4eb92c58200932eba7a6d74b961a52c804f
gerom <gerome.bernabe@ucalgary.ca>
2022-11-17 23:46:36 -0700

    Created an exception class

---
+++ src/application/InvalidCurrencyException.java
@@ -0,0 +1,30 @@
+package application;
+
+public class InvalidCurrencyException extends Exception {
+
+	public InvalidCurrencyException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidCurrencyException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidCurrencyException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidCurrencyException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidCurrencyException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
