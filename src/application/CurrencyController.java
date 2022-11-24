3c05fa3dbe548bf195fa3c8874ba0a2efcf7cc3a
HugoAmador <hugo.amador@ucalgary.ca>
2022-11-24 08:17:23 +0000

    Created the Multiple Conversions choice boxes and added the button for the fun facts

--- src/application/CurrencyController.java
+++ src/application/CurrencyController.java
@@ -47,6 +47,8 @@ public class CurrencyController {
 			TESTLabel.setText(money.toString());
 		}
 	}
+	
+	//void printRandomFunFact(Scene mainScene, );
 		
 	@FXML
     void funFact (ActionEvent getFunFacts){
@@ -55,7 +57,7 @@ public class CurrencyController {
     	
     	//Sets new window
 		VBox funFacts = new VBox();
-		Scene scene = new Scene(funFacts,290,550);
+		Scene scene = new Scene(funFacts,300,400);
 		funFacts.setAlignment(Pos.CENTER);
 		applicationStage.setScene(scene);
 		applicationStage.setTitle("Fun Facts");
@@ -73,13 +75,22 @@ public class CurrencyController {
     	chooseCurrency.setPadding(new Insets (1,1,1,1));
     	chooseCurrency.setAlignment(Pos.CENTER);
     	
-    	Label chooseLabel = new Label ("Choose a Currency: ");
+    	Label chooseLabel = new Label ("Press to get a random fun fact: ");
     	chooseCurrency.getChildren().add(chooseLabel);
     	funFacts.getChildren().add(chooseCurrency);
     	
     	
     	
-    	Button doneButton = new Button ("Done");  
+    	
+    	
+    	
+    	Button funButton = new Button ("Fun Fact");
+    	funFacts.getChildren().add(funButton);
+    	Label blankSpace2 = new Label ("");
+    	funFacts.getChildren().add(blankSpace2);
+    	
+    	Button doneButton = new Button ("Done");
+    	//doneButton.setOnAction(finishEvent -> funFact(mainScene, funFacts));
     	funFacts.getChildren().add(doneButton);
 
 		
@@ -109,10 +120,69 @@ public class CurrencyController {
     	HBox choseCurrency = new HBox(); 
     	choseCurrency.setPadding(new Insets (1,1,1,1));
     	
-    	Label choseLabel = new Label ("Chose a Currency: ");
+    	Label choseLabel = new Label ("Chose a Currency you want to convert: ");
     	choseCurrency.getChildren().add(choseLabel);
+    	Label blankSpace3 = new Label ("");
+    	multipleCurrency.getChildren().add(blankSpace3);
     	
-    	multipleCurrency.getChildren().add(choseCurrency);
+    	ChoiceBox<String> chooseChoiceBox = new ChoiceBox<String>();
+    	chooseChoiceBox.getItems().add("US Dollar (USD)");
+    	chooseChoiceBox.getItems().add("Euro (EUR)");
+    	chooseChoiceBox.getItems().add("Japanese Yen (JPY)");
+    	chooseChoiceBox.getItems().add("Pound Sterling (GBP)");
+    	chooseChoiceBox.getItems().add("Australian Dollar (AUD)");
+    	chooseChoiceBox.getItems().add("Canadian Dollar (CAD)");
+    	chooseChoiceBox.getItems().add("Swiss Franc (CHF)");
+    	chooseChoiceBox.getItems().add("Chinese Renminbi (CNH)");
+    	chooseChoiceBox.getItems().add("Hong Kong Dollar (HKD))");
+    	chooseChoiceBox.getItems().add("New Zealand Dollar (NZD)");
+    	
+    	choseCurrency.getChildren().add(chooseChoiceBox);
+    	
+    	// Contains Label and ChoiceBox 2
+    	HBox choseCurrencyToConvert = new HBox(); 
+    	choseCurrencyToConvert.setPadding(new Insets (1,1,1,1));
+    	
+    	Label choseToLabel = new Label ("Chose the first Currency you want to convert to: ");
+    	choseCurrencyToConvert.getChildren().add(choseToLabel);
+    	
+    	ChoiceBox<String> chooseToChoiceBox = new ChoiceBox<String>();
+    	chooseToChoiceBox.getItems().add("US Dollar (USD)");
+    	chooseToChoiceBox.getItems().add("Euro (EUR)");
+    	chooseToChoiceBox.getItems().add("Japanese Yen (JPY)");
+    	chooseToChoiceBox.getItems().add("Pound Sterling (GBP)");
+    	chooseToChoiceBox.getItems().add("Australian Dollar (AUD)");
+    	chooseToChoiceBox.getItems().add("Canadian Dollar (CAD)");
+    	chooseToChoiceBox.getItems().add("Swiss Franc (CHF)");
+    	chooseToChoiceBox.getItems().add("Chinese Renminbi (CNH)");
+    	chooseToChoiceBox.getItems().add("Hong Kong Dollar (HKD))");
+    	chooseToChoiceBox.getItems().add("New Zealand Dollar (NZD)");
+    	
+    	choseCurrencyToConvert.getChildren().add(chooseToChoiceBox);
+    	
+    	// Contains Label and ChoiceBox 3
+    	HBox choseCurrencyToConvert2 = new HBox(); 
+    	choseCurrencyToConvert.setPadding(new Insets (1,1,1,1));
+    	
+    	Label choseToLabel2 = new Label ("Chose the second Currency you want to convert to: ");
+    	choseCurrencyToConvert2.getChildren().add(choseToLabel2);
+    	
+    	ChoiceBox<String> chooseToChoiceBox2 = new ChoiceBox<String>();
+    	chooseToChoiceBox2.getItems().add("US Dollar (USD)");
+    	chooseToChoiceBox2.getItems().add("Euro (EUR)");
+    	chooseToChoiceBox2.getItems().add("Japanese Yen (JPY)");
+    	chooseToChoiceBox2.getItems().add("Pound Sterling (GBP)");
+    	chooseToChoiceBox2.getItems().add("Australian Dollar (AUD)");
+    	chooseToChoiceBox2.getItems().add("Canadian Dollar (CAD)");
+    	chooseToChoiceBox2.getItems().add("Swiss Franc (CHF)");
+    	chooseToChoiceBox2.getItems().add("Chinese Renminbi (CNH)");
+    	chooseToChoiceBox2.getItems().add("Hong Kong Dollar (HKD))");
+    	chooseToChoiceBox2.getItems().add("New Zealand Dollar (NZD)");
+    	
+    	choseCurrencyToConvert2.getChildren().add(chooseToChoiceBox2);
+    	
+    	
+    	multipleCurrency.getChildren().addAll(choseCurrency, choseCurrencyToConvert, choseCurrencyToConvert2 );
     	
     	
     	//return to main scene
