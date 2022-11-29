2bda4d9588e20858ea8a9369076cf6d40ae1097a
HugoAmador <hugo.amador@ucalgary.ca>
2022-11-29 07:54:17 +0000

    Upgraded GUI for facts window, and created it's controller

---
+++ src/application/FunFactsController.java
@@ -0,0 +1,18 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class FunFactsController {
+
+	@FXML
+	private Label factLabel;
+	
+
+	@FXML
+	void getFact(ActionEvent event) {
+		
+	}
+
+}
