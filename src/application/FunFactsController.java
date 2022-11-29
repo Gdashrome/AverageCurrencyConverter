72e7ab4df220ff296ce826b590b34a4d9d9d288b
HugoAmador <hugo.amador@ucalgary.ca>
2022-11-29 08:26:33 +0000

    Tried to print the facts with a random number, just a little bit to go.

--- src/application/FunFactsController.java
+++ src/application/FunFactsController.java
@@ -13,6 +13,15 @@ public class FunFactsController {
 	@FXML
 	void getFact(ActionEvent event) {
 		
+		double num = Math.floor(Math.random() * 2) +1;
+		System.out.println(num);
+		
+		if (num == 1) {
+			factLabel.setText("US dollar fun fact: " + "The Federal Reserve spends about 6 cents to produce every $1 bill.");
+		}
+		else if (num == 2) {
+			factLabel.setText("Euro fun fact: " + "<br>" + "It is estimated that between 20% and 25% of the euro banknotes in circulation are held outside the euro area, mainly in neighboring countries.");
+		}
 	}
 
 }
